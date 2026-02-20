6b52e9106e7c4f63fbd6c8c2d755397857be864e
Anshul Sinha <sinha.anshul1@gmail.com>
2026-02-19 22:00:58 -0800

    Added OSMNX and networkx libraries

--- city/city.py
+++ city/city.py
@@ -3,87 +3,54 @@ from .edge import Edge
 import random
 import matplotlib.pyplot as plt
 from vehicles import *
+import osmnx as ox
+import networkx as nx
+from termcolor import colored
 
 class City:
-    def __init__(self, nodes={}, edges=[], width=1, height=1, vehicles=[]):
+    def __init__(self, vehicles=[]):
         # CHAGNE TO GRAPH ADJACENCY LIST REPRESNTATION
-        self.nodes = nodes
-        self.edges = edges
-        self.width = width
-        self.height = height
+        self.graph = None
         self.vehicles = vehicles
     
-    def add_node(self, node):
-        self.nodes[node.id] = node
+    def load_city_from_address(self, address, radius=2000):
+        self.graph = ox.graph_from_address(address, radius, network_type="drive")
     
-    def add_edge(self, edge):
-        self.edges.append(edge)
+    def load_city_from_place(self, location):
+        self.graph = ox.graph_from_place(location, network_type="drive")
+    
+    def add_time_to_edges(self):
+        for edge in self.graph.edges(keys=True, data=True):
+            try:
+                from_node = edge[0]
+                to_node = edge[1]
+                key = edge[2]
+                data = self.graph.edges[from_node, to_node, key]
+                speed = int(data['maxspeed'].split()[0])
+                distance = data['length']
+                self.graph.edges[from_node, to_node, key]['time'] = distance / speed
+                print(edge)
+            except KeyError:
+                print(colored(f"Keys for edge: {str(self.graph.edges[from_node, to_node, key].keys())}", "red"))
 
-    def save_city(self, filename="city.txt"):
-        with open(filename, 'w') as f:
-            for node in self.nodes.values():
-                f.write(f"NODE {node.id} {node.x} {node.y}\n")
-            for edge in self.edges:
-                f.write(f"EDGE {edge.start_node.id} {edge.end_node.id} {edge.weight}\n")
-    
-    def load_city(self, filename="city.txt"):
-        with open(filename, 'r') as f:
-            for line in f:
-                parts = line.strip().split()
-                if parts[0] == "NODE":
-                    _, id, x, y = parts
-                    self.add_node(Node(id, float(x), float(y)))
-                elif parts[0] == "EDGE":
-                    _, start_id, end_id, weight = parts
-                    start_node = self.nodes[start_id]
-                    end_node = self.nodes[end_id]
-                    self.add_edge(Edge(start_node, end_node, float(weight)))
-    
-    def generate_random_city(self, num_nodes, num_edges, num_vehicles=10):
-        for i in range(num_nodes):
-            x = random.uniform(0, self.width)
-            y = random.uniform(0, self.height)
-            self.add_node(Node(str(i), x, y))
+    def get_route(self, start, end):
+        orig = ox.distance.nearest_nodes(self.graph, start[1], start[0])
+        dest = ox.distance.nearest_nodes(self.graph, end[1], end[0])
+        return nx.shortest_path(self.graph, orig, dest, weight='time')
         
-        for _ in range(num_edges):
-            start_node = random.choice(list(self.nodes.values()))
-            end_node = random.choice(list(self.nodes.values()))
-            while end_node == start_node:
-                end_node = random.choice(list(self.nodes.values()))
-            weight = ((start_node.x - end_node.x) ** 2 + (start_node.y - end_node.y) ** 2) ** 0.5
-            self.add_edge(Edge(start_node, end_node, weight))
-        
-        for i in range(num_vehicles):
-            self.vehicles.append(Vehicle(str(i), random.uniform(0, self.width), random.uniform(0, self.height)))
-    
-    def generate_grid_city(self, width, height, width_interval, heigh_interval):
-        node_id = 0
-        self.width = width
-        self.height = height
-        for x in range(0, width, width_interval):
-            for y in range(0, height, heigh_interval):
-                self.add_node(Node(str(node_id), x, y))
-                node_id += 1
-            
-        
-    
     def visualize_city(self):
-        plt.figure(figsize=(self.width * 10, self.height * 10))
-        for edge in self.edges:
-            x_values = [edge.start_node.x, edge.end_node.x]
-            y_values = [edge.start_node.y, edge.end_node.y]
-            plt.plot(x_values, y_values, 'k-', lw=0.5)
-        
-        for node in self.nodes.values():
-            plt.plot(node.x, node.y, 'ro')
-
-        for vehicle in self.vehicles:
-            plt.plot(vehicle.x, vehicle.y, color=vehicle.color, marker='s', markersize=10)
-        
-        plt.title("City Visualization")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.grid()
+        if self.graph is None:
+            print("No graph to visualize. Please load a city first.")
+            return
+        fig, ax = ox.plot.plot_graph_route(
+            self.graph,
+            route_color='r',
+            route_linewidth=4,
+            route_alpha=0.6,
+            orig_dest_size=100,
+            show=True,
+            close=True
+        )
         plt.show()
 
 if __name__ == "__main__":
--- city/edge.py
+++
@@ -1,8 +0,0 @@
-class Edge:
-    def __init__(self, start_node, end_node, weight):
-        self.start_node = start_node
-        self.end_node = end_node
-        self.weight = weight
-    
-    def __str__(self):
-        return f"Edge({self.start_node} -> {self.end_node}, weight={self.weight})"
